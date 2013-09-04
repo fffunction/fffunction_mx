afcf2f4a951e135f128ad9b676c4076fcd33f933
Adam Robertson <adam@fffunction.co>
2013-09-04 18:35:46 +0100

    Removed gutters from the grid as GridSet no place nicely

--- assets/gridset/gridset-overlay.js
+++ assets/gridset/gridset-overlay.js
@@ -15,10 +15,10 @@ gs = {
 				switch (e.which || e.keyCode) {
 					case 71:
 					
-						var gw = document.getElementById('gridsetoverlaywrap');
+						var gw = document.querySelectorAll('.gridsetoverlaywrap, #gridsetoverlaystyles, #gridscreenwidthwrap');
 					
-						if (!gw) gs.show();
-						else gs.remove(gw);
+						if (gw.length == 0) gs.show();
+						else { window.location.href = window.location.href.replace('?gridset=show', '') }
 						
 						gs.prevent(e);
 						break;
@@ -32,15 +32,6 @@ gs = {
 	
 	},
 	
-	remove: function (gw) {
-	
-		document.body.removeChild(gw);
-		
-		if(window.detachEvent) window.detachEvent('onresize', gs.width);
-		else window.removeEventListener('resize', gs.width, false);
-	
-	},
-	
 	width: function () {
 		
 		var swv = document.getElementById('gridscreenwidthval');
@@ -50,60 +41,133 @@ gs = {
 
 	show: function () {
 	
-		var p = ['da','db','t','m'],
-			c = [12,8,8,2],
-			w = [1024,990,768,320],
-			b = document.getElementsByTagName('body')[0],
-			gw = '<div id="gridwrap"><div id="gridscreenwidthwrap"><p id="gridscreenwidth">Screen width: <strong id="gridscreenwidthval"></strong></p></div><div id="gridoverlay" class="wrapper">',
-		
-			k = 0, breaks = '',
+		var b = document.getElementsByTagName('body')[0],
+				gridareas = document.querySelectorAll('[class*=-showgrid]'),
+				areacount = gridareas.length,
+				wrapper = document.querySelectorAll('.wrapper'),
 			
-			styles = '<style id="gridsetoverlaystyles" type="text/css">#gridsetoverlaywrap{position:static;}#gridwrap{display:block;position:fixed;top:0;left:0;width:100%;height:100%;z-index:1000;pointer-events:none;font-family:Helvetica, Arial, sans-serif !important;}#gridoverlay{position:relative;height:100%;overflow:hidden !important;background:none !important;}#gridoverlay div{display:block;position:static;height:100%;color:#bcbcff;}#gridoverlay .gridset{position:absolute;width:100%;height:100%;top:0;left:0;opacity:0.7;}#gridoverlay .gridset div{text-align:left;font-size:10px !important;border-right:1px solid #bcbcff;border-left:1px solid #bcbcff;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;box-sizing:border-box;}#gridoverlay div small{width:100%;display:block;text-align:center;color:#7D80DB;font-weight:700 !important;border-bottom:1px solid #bcbcff;border-top:1px solid #bcbcff;padding-top:0 !important;background-color:rgb(240,240,255) !important;text-transform:none !important;height:22px !important;line-height:22px !important;text-style:normal !important;}#gridoverlay .gridset:nth-child(2) div{border-style:dashed;padding-top:23px;}#gridoverlay .gridset:nth-child(2) small{border-style:dashed;}#gridoverlay .gridset:nth-child(3) div{border-style:dotted;padding-top:45px;}#gridoverlay .gridset:nth-child(3) small{border-style:dotted;}#gridsetoverlaywrap .noshow{display:none;}#gridscreenwidthwrap{display:block !important;width:100% !important;position:absolute !important;bottom:0 !important;left:0 !important;height:30px !important;border-top:1px solid #7D80DB !important;opacity:0.7 !important;background-color:rgb(240,240,255) !important;}#gridscreenwidth{display:block !important;width:100% !important;text-align:center !important;font-size:12px !important;line-height:1 !important;padding-top:8px !important;font-family:Helvetica, Arial, sans-serif !important; margin: 0 !important;color:#7D80DB !important;}@media only screen and (max-width:767px) {#gridsetoverlaywrap [class*=m1],#gridsetoverlaywrap [class*=m2],#gridsetoverlaywrap .m-all{display:block;float:left;margin-right:-100%;}#gridsetoverlaywrap [class*=m1]{width:48.4375%;margin-left:0%;}#gridsetoverlaywrap [class*=m2]{width:48.4375%;margin-left:51.5625%;}#gridsetoverlaywrap .m-hide{display:none !important;}}@media only screen and (min-width:768px) and (max-width:989px) {#gridsetoverlaywrap [class*=t1],#gridsetoverlaywrap [class*=t2],#gridsetoverlaywrap [class*=t3],#gridsetoverlaywrap [class*=t4],#gridsetoverlaywrap [class*=t5],#gridsetoverlaywrap [class*=t6],#gridsetoverlaywrap [class*=t7],#gridsetoverlaywrap [class*=t8],#gridsetoverlaywrap .t-all{display:block;float:left;margin-right:-100%;}#gridsetoverlaywrap [class*=t1]{width:11.528620429525528%;margin-left:0%;}#gridsetoverlaywrap [class*=t2]{width:11.528620429525528%;margin-left:14.132787099526%;}#gridsetoverlaywrap [class*=t3]{width:11.528620429525528%;margin-left:28.265574199051%;}#gridsetoverlaywrap [class*=t4]{width:11.528620429525528%;margin-left:42.398361298577%;}#gridsetoverlaywrap [class*=t5]{width:8.962362548325935%;margin-left:56.531148398102%;}#gridsetoverlaywrap [class*=t6]{width:8.919110781966982%;margin-left:68.097677616428%;}#gridsetoverlaywrap [class*=t7]{width:8.875201471647912%;margin-left:79.620955068395%;}#gridsetoverlaywrap [class*=t8]{width:8.834572646623739%;margin-left:91.100323210043%;}#gridsetoverlaywrap .t-hide{display:none !important;}}@media only screen and (min-width:990px) and (max-width:1023px) {#gridsetoverlaywrap [class*=db1],#gridsetoverlaywrap [class*=db2],#gridsetoverlaywrap [class*=db3],#gridsetoverlaywrap [class*=db4],#gridsetoverlaywrap [class*=db5],#gridsetoverlaywrap [class*=db6],#gridsetoverlaywrap [class*=db7],#gridsetoverlaywrap [class*=db8],#gridsetoverlaywrap .db-all{display:block;float:left;margin-right:-100%;}#gridsetoverlaywrap [class*=db1]{width:12.145045497736335%;margin-left:0%;}#gridsetoverlaywrap [class*=db2]{width:12.145045497736335%;margin-left:14.165247517736%;}#gridsetoverlaywrap [class*=db3]{width:12.145045497736335%;margin-left:28.330495035473%;}#gridsetoverlaywrap [class*=db4]{width:12.145045497736335%;margin-left:42.495742553209%;}#gridsetoverlaywrap [class*=db5]{width:9.392168515078767%;margin-left:56.660990070945%;}#gridsetoverlaywrap [class*=db6]{width:9.334248499173963%;margin-left:68.073360606024%;}#gridsetoverlaywrap [class*=db7]{width:9.277349639316233%;margin-left:79.427811125198%;}#gridsetoverlaywrap [class*=db8]{width:9.22413216356651%;margin-left:90.725362784514%;}#gridsetoverlaywrap .db-hide{display:none !important;}}@media only screen and (min-width:1024px) {#gridsetoverlaywrap [class*=da1],#gridsetoverlaywrap [class*=da2],#gridsetoverlaywrap [class*=da3],#gridsetoverlaywrap [class*=da4],#gridsetoverlaywrap [class*=da5],#gridsetoverlaywrap [class*=da6],#gridsetoverlaywrap [class*=da7],#gridsetoverlaywrap [class*=da8],#gridsetoverlaywrap [class*=da9],#gridsetoverlaywrap [class*=da10],#gridsetoverlaywrap [class*=da11],#gridsetoverlaywrap [class*=da12],#gridsetoverlaywrap .da-all{display:block;float:left;margin-right:-100%;}#gridsetoverlaywrap [class*=da1]{width:7.20304513090609%;margin-left:0%;}#gridsetoverlaywrap [class*=da2]{width:7.20304513090609%;margin-left:9.1561701309061%;}#gridsetoverlaywrap [class*=da3]{width:7.20304513090609%;margin-left:18.312340261812%;}#gridsetoverlaywrap [class*=da4]{width:7.20304513090609%;margin-left:27.468510392718%;}#gridsetoverlaywrap [class*=da5]{width:7.20304513090609%;margin-left:36.624680523624%;}#gridsetoverlaywrap [class*=da6]{width:7.20304513090609%;margin-left:45.78085065453%;}#gridsetoverlaywrap [class*=da7]{width:5.953537297735504%;margin-left:54.937020785437%;}#gridsetoverlaywrap [class*=da8]{width:5.922484600344462%;margin-left:62.843683083172%;}#gridsetoverlaywrap [class*=da9]{width:5.88991796390554%;margin-left:70.719292683517%;}#gridsetoverlaywrap [class*=da10]{width:5.858389813165863%;margin-left:78.562335647422%;}#gridsetoverlaywrap [class*=da11]{width:5.827455213434061%;margin-left:86.373850460588%;}#gridsetoverlaywrap [class*=da12]{width:5.796741200978035%;margin-left:94.154430674022%;}#gridsetoverlaywrap .da-hide{display:none !important;}}</style>';
-						
-		while (p[k]) {
+				styles = '.gridsetoverlaywrap{padding:0 !important;display:block;position:absolute;top:0;left:0;width:100%;height:100%;z-index:10000;pointer-events:none;}.gridsetnoareas .gridsetoverlaywrap{position:fixed;}.gridwrap{padding:0 !important;display:block;position:absolute;top:0;left:0;width:100%;height:100%;font-family:Helvetica, Arial, sans-serif !important;}.gridoverlay{padding:0 !important;position:relative;height:100%;overflow:hidden !important;background:none !important;}.gridoverlay .gridset{padding:0 !important;position:absolute;width:100%;height:100%;top:0;left:0;opacity:0.8; display:block;}.gridoverlay .gridset div{padding:0;text-align:left;font-size:10px !important;border:1px solid #FFD800 !important;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;box-sizing:border-box;height:100%;}.gridoverlay .gridset > div{border:none !important;height:100%;position:absolute;top:0;left:0;width:100%;}.gridoverlay div small{width:100%;display:block;text-align:center;font-weight:400 !important;letter-spacing: 1px !important;padding-top:0 !important;text-transform:none !important;height:22px !important;line-height:22px !important;text-style:normal !important;border-bottom:1px solid #FFD800 !important;color:#333 !important;background-color:#FFF79F !important;}.gridsetnoareas .gridoverlay .gridset > div:nth-child(2){padding-top:23px !important;}.gridsetnoareas .gridoverlay .gridset > div:nth-child(2) small{border-bottom:1px dashed #FFD800 !important;}.gridsetnoareas .gridoverlay .gridset > div:nth-child(2) > div{border:1px dashed #FFD800 !important;}.gridsetnoareas .gridoverlay .gridset > div:nth-child(3){padding-top:45px !important;}.gridsetnoareas .gridoverlay .gridset > div:nth-child(3) small{border-bottom:1px dotted #FFD800 !important;}.gridsetnoareas .gridoverlay .gridset > div:nth-child(3) > div{border:1px dotted #FFD800 !important;}.gridsetnoareas .gridoverlay .gridset > div:nth-child(4){padding-top:67px !important;}.gridsetnoareas .gridoverlay .gridset > div:nth-child(4) small{border-bottom:1px double #FFD800 !important;}.gridsetnoareas .gridoverlay .gridset > div:nth-child(4) > div{border:1px double #FFD800 !important;}.gridsetoverlaywrap .noshow{display:none;}#gridscreenwidthwrap{margin:0 !important;padding:0 !important;display:none;width:100%;position:fixed !important;z-index:10000 !important;bottom:0 !important;left:0 !important;height:30px !important;opacity:0.95;border-top:1px solid #FFD800 !important;color:#333;background-color:#FFF79F !important;font-family:Helvetica, Arial, sans-serif !important;}#gridscreenwidth{margin:0 !important;display:block;width:100% !important;max-width:none !important;text-align:center !important;font-size:12px;line-height:1;padding-top:8px !important;}#gridscreenwidth strong{text-transform:none;}',
+				
+				newstyles = document.createElement('style'),
+				newwidth = document.createElement('div'),
+				head = document.getElementsByTagName('head'),
+				newfavicon = document.createElement('link'),
+				newgsstyles = document.createElement('link');
 		
-			var hides = '', 
-				l = 0;
+		newstyles.id = 'gridsetoverlaystyles';
+		newstyles.innerHTML = styles;
+		newstyles.type = 'text/css';
 		
-			if (w[k] != breaks && k == 0) gw += '<div>';
-			else if (w[k] != breaks) gw += '</div><div>';
+		newwidth.id = 'gridscreenwidthwrap';
+		newwidth.innerHTML = '<p id="gridscreenwidth">Screen width: <strong id="gridscreenwidthval"></strong></p>';
 		
-			while (p[l]) {
+		b.appendChild(newstyles);
+		b.appendChild(newwidth);
 		
-				if (l != k && w[l] != w[k]) hides += p[l] + '-hide ';
-				l++;			
+		var newwidthdisplay = (newwidth.currentStyle) ? newwidth.currentStyle.display : getComputedStyle(newwidth, null).display;
 		
-			}
+		newfavicon.rel = "shortcut icon";
+		newfavicon.id = "gridsetfavicon";
+		newfavicon.href = "http://dev.gridsetapp.com/app/img/favicon.ico";
 		
-			gw += '<div class="gridset ' + hides + '"><div class="'+p[k]+'1"><small>'+p[k]+'1</small></div>';
+		head[0].appendChild(newfavicon);
 		
-			var i = 1;
+		if (newwidthdisplay != 'block') {
 		
-			while (i++ < c[k]) gw += '<div class="'+p[k]+i+'"><small>'+p[k]+i+'</small></div>';
-		
-			gw += '</div>';
-		
-			if (k == w.length - 1) gw += '</div>';
-		
-			breaks = w[k];
-		
-			k++;
+			newgsstyles.rel = "stylesheet";
+			newgsstyles.id = "gridsetstyles";
+			newgsstyles.href = "https://get.gridsetapp.com/18282/";
+			head[0].appendChild(newgsstyles);
 		
 		}
 		
-		gw += '</div></div>';
+		if (areacount) {
+			
+			var j = areacount;
+			
+			while (j-- > 0) {
+			
+				var area = gridareas[j];
+			
+				gs.buildgrid(area, j, areacount);
+				
+				if (window.getComputedStyle(area,null).getPropertyValue("position") == 'static') area.style.position = 'relative';
+				
+			}
+			
+		}
+		else {
+			
+			if (!b.className.match('gridsetnoareas')) b.className += ' gridsetnoareas';
+			
+			gs.buildgrid(b, j, areacount);
 		
-		var newgw = document.createElement('div');
-		
-		newgw.id = 'gridsetoverlaywrap';
-		
-		newgw.innerHTML = gw + styles;
-		
-		b.appendChild(newgw);
+		}
 		
 		gs.width();
 		gs.bind(window, 'resize', gs.width);
 	
+	},
+	
+	buildgrid: function (area, j, showgrid) {
+		
+		var set = JSON.parse('{"id":"18282","name":"prototyping","widths":{"1024":{"width":1024,"grids":{"da":{"name":"Desktop A","prefix":"da","width":1024,"columns":{"da1":{"name":"da1","unit":"%","percent":8.32926432,"px":85.29},"da2":{"name":"da2","unit":"%","percent":8.32926432,"px":85.29},"da3":{"name":"da3","unit":"%","percent":8.32926432,"px":85.29},"da4":{"name":"da4","unit":"%","percent":8.32926432,"px":85.29},"da5":{"name":"da5","unit":"%","percent":8.32926432,"px":85.29},"da6":{"name":"da6","unit":"%","percent":8.32926432,"px":85.29},"da7":{"name":"da7","unit":"%","percent":8.32926432,"px":85.29},"da8":{"name":"da8","unit":"%","percent":8.32926432,"px":85.29},"da9":{"name":"da9","unit":"%","percent":8.32926432,"px":85.29},"da10":{"name":"da10","unit":"%","percent":8.32926432,"px":85.29},"da11":{"name":"da11","unit":"%","percent":8.32926432,"px":85.29},"da12":{"name":"da12","unit":"%","percent":8.32926432,"px":85.29}},"gutter":{"unit":"px","px":0,"percent":0},"ratio":{"name":"even","value":1}}}},"990":{"width":990,"grids":{"db":{"name":"Desktop B","prefix":"db","width":990,"columns":{"db1":{"name":"db1","unit":"%","percent":12.49368687,"px":123.69},"db2":{"name":"db2","unit":"%","percent":12.49368687,"px":123.69},"db3":{"name":"db3","unit":"%","percent":12.49368687,"px":123.69},"db4":{"name":"db4","unit":"%","percent":12.49368687,"px":123.69},"db5":{"name":"db5","unit":"%","percent":12.49368687,"px":123.69},"db6":{"name":"db6","unit":"%","percent":12.49368687,"px":123.69},"db7":{"name":"db7","unit":"%","percent":12.49368687,"px":123.69},"db8":{"name":"db8","unit":"%","percent":12.49368687,"px":123.69}},"gutter":{"unit":"px","px":0,"percent":0},"ratio":{"name":"even","value":1}}}},"768":{"width":768,"grids":{"t":{"name":"Tablet","prefix":"t","width":768,"columns":{"t1":{"name":"t1","unit":"%","percent":12.49186198,"px":95.94},"t2":{"name":"t2","unit":"%","percent":12.49186198,"px":95.94},"t3":{"name":"t3","unit":"%","percent":12.49186198,"px":95.94},"t4":{"name":"t4","unit":"%","percent":12.49186198,"px":95.94},"t5":{"name":"t5","unit":"%","percent":12.49186198,"px":95.94},"t6":{"name":"t6","unit":"%","percent":12.49186198,"px":95.94},"t7":{"name":"t7","unit":"%","percent":12.49186198,"px":95.94},"t8":{"name":"t8","unit":"%","percent":12.49186198,"px":95.94}},"gutter":{"unit":"px","px":0,"percent":0},"ratio":{"name":"even","value":1}}}},"320":{"width":320,"grids":{"m":{"name":"Mobile","prefix":"m","width":320,"columns":{"m1":{"name":"m1","unit":"%","percent":49.921875,"px":159.75},"m2":{"name":"m2","unit":"%","percent":49.921875,"px":159.75}},"gutter":{"unit":"px","px":0,"percent":0},"ratio":{"name":"even","value":1}}}}},"prefixes":{"index":["da","db","t","m"],"1024":["da"],"990":["db"],"768":["t"],"320":["m"]}}'),
+		
+				gridwrap = document.createElement('div'),
+				gridinner = (showgrid) ? '<div class="gridwrap"><div class="gridoverlay">' : '<div class="gridwrap"><div class="gridoverlay wrapper">',
+				
+				awidth = area.clientWidth,
+				apadleft = (parseFloat(gs.getstyle(area, 'padding-left')) / awidth) * 100,
+				apadright = (parseFloat(gs.getstyle(area, 'padding-right')) / awidth) * 100;
+		
+		if (showgrid) gridwrap.className = 'gridsetoverlaywrap';
+		else gridwrap.className = 'gridsetoverlaywrap';
+		
+		for (w in set.widths) {
+			
+			var width = set.widths[w],
+					hides = '';
+			
+			for (p in set.prefixes) {
+				
+				if (p != w && p != 'index') hides += set.prefixes[p][0] + "-hide ";
+				
+			}
+			
+			gridinner += '<div class="gridset ' + hides + '">';
+			
+			for (j in width.grids) {
+			
+				var grid = width.grids[j],
+						showreg = new RegExp('(^| )' + grid.prefix + '-showgrid( |$)');
+				
+				if (!showgrid || area.className.match(showreg)) {
+				
+					gridinner += '<div style="padding-left:' + apadleft + '%;padding-right:' + apadright + '%;">';
+					
+					for (k in grid.columns) {
+						
+						var col = grid.columns[k];
+						
+						gridinner += '<div class="' + col.name + '"><small>' + col.name + '</small></div>';
+					
+					}
+					
+					gridinner += '</div>';
+				
+				}
+			}
+			
+			gridinner += '</div>';
+		
+		}
+		
+		gridinner += '</div></div>';
+		
+		gridwrap.innerHTML = gridinner;
+		
+		area.appendChild(gridwrap);
+		
 	},
 	
 	bind : function (t, e, f) {
@@ -118,6 +182,14 @@ gs = {
 		if (e.preventDefault) e.preventDefault();
 		else event.returnValue = false;
 	
+	},
+	
+	getstyle : function (t, p){
+	
+	 if (t.currentStyle) return t.currentStyle[p];
+	 else if (document.defaultView && document.defaultView.getComputedStyle) return document.defaultView.getComputedStyle(t, "").getPropertyValue(p);
+	 else return t.style[p];
+	 
 	}
 
 
